9043282f6ab072a4f5612bc5b8782b5e345158b0
Tomas Trajan <tomas.trajan@gmail.com>
2015-05-03 17:49:50 +0200

    style - add jscs support (google style), reformatted code, removed unused requires

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,85 +8,86 @@
 
 'use strict';
 
-module.exports = function (grunt) {
-    // load all npm grunt tasks
-    require('load-grunt-tasks')(grunt);
+module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
+  require('load-grunt-tasks')(grunt);
 
-        // Lint JavaScript
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'tasks/**/*.js',
-                '<%= nodeunit.test %>'
-            ],
-            options: {
-                jshintrc: '.jshintrc',
-                reporter: require('jshint-stylish')
-            }
+  grunt.initConfig({
+
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/**/*.js',
+        '<%= nodeunit.test %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      }
+    },
+
+    jscs: {
+      src: ['Gruntfile.js', 'tasks/**/*.js', 'test/*.js'],
+      options: {
+        config: '.jscsrc',
+        verbose: true,
+        fix: true
+      }
+    },
+
+    clean: {
+      test: ['tmp']
+    },
+
+    copy: {
+      test: {
+        files: [
+          {cwd: 'test/fixtures/', expand: true, src: ['**/*.*'], dest: 'tmp/basic/'},
+          {cwd: 'test/fixtures/', expand: true, src: ['**/*.*'], dest: 'tmp/full/'},
+          {cwd: 'test/fixtures/', expand: true, src: ['**/*.*'], dest: 'tmp/notFoundSrc/'},
+          {cwd: 'test/fixtures/', expand: true, src: ['**/*.*'], dest: 'tmp/notFoundDest/'}
+        ]
+      }
+    },
+
+    ngsrc: {
+
+      basic: {
+        src: ['tmp/basic/src/**/*.js', '!tmp/basic/src/**/*.spec.js'],
+        dest: ['tmp/basic/**/*.html']
+      },
+      full: {
+        options: {
+          moduleDiscriminator: '.module.js',
+          path: 'path/to/files/after/build/process/',
+          placeholder: '<!-- my-custom-placeholder-regex -->'
         },
+        cwd: 'tmp/full/src/',
+        src: ['**/*.js', '!**/*.spec.js'],
+        dest: ['tmp/full/**/*.html']
+      },
+      notFoundSrc: {
+        src: ['bad/path/**/*.js', '!bad/path/**/*.spec.js'],
+        dest: ['tmp/notFoundSrc/**/*.html']
+      },
+      notFoundDest: {
+        src: ['tmp/notFoundDest/**/*.js', '!tmp/notFoundDest/**/*.spec.js'],
+        dest: ['temp/path/**/*.html']
+      }
+    },
 
-        // Before generating any new files, remove any previously-created files.
-        clean: {
-            test: ['tmp']
-        },
+    nodeunit: {
+      test: ['test/*.spec.js']
+    }
 
-        // Copy test fixtures to tmp folder before replacing placeholders to preserve test data
-        copy: {
-            test: {
-                files: [
-                    { cwd: 'test/fixtures/', expand: true, src: ['**/*.*'], dest: 'tmp/basic/' },
-                    { cwd: 'test/fixtures/', expand: true, src: ['**/*.*'], dest: 'tmp/full/' },
-                    { cwd: 'test/fixtures/', expand: true, src: ['**/*.*'], dest: 'tmp/notFoundSrc/' },
-                    { cwd: 'test/fixtures/', expand: true, src: ['**/*.*'], dest: 'tmp/notFoundDest/' }
-                ]
-            }
-        },
+  });
 
-        // Configuration to be run (and then tested).
-        ngsrc: {
+  grunt.loadTasks('tasks');
 
-            basic: {
-                src: ['tmp/basic/src/**/*.js', '!tmp/basic/src/**/*.spec.js'],
-                dest: ['tmp/basic/**/*.html']
-            },
-            full: {
-                options: {
-                    moduleDiscriminator: '.module.js',
-                    path: 'path/to/files/after/build/process/',
-                    placeholder: '<!-- my-custom-placeholder-regex -->'
-                },
-                cwd: 'tmp/full/src/',
-                src: ['**/*.js', '!**/*.spec.js'],
-                dest: ['tmp/full/**/*.html']
-            },
-            notFoundSrc: {
-                src: ['bad/path/**/*.js', '!bad/path/**/*.spec.js'],
-                dest: ['tmp/notFoundSrc/**/*.html']
-            },
-            notFoundDest: {
-                src: ['tmp/notFoundDest/**/*.js', '!tmp/notFoundDest/**/*.spec.js'],
-                dest: ['temp/path/**/*.html']
-            }
-        },
+  grunt.registerTask('lint', ['jshint', 'jscs']);
 
-        // Unit tests.
-        nodeunit: {
-            test: ['test/*.spec.js']
-        }
+  grunt.registerTask('test', ['clean', 'copy', 'ngsrc', 'nodeunit']);
 
-    });
-
-    // Actually load this plugin's task(s).
-    grunt.loadTasks('tasks');
-
-    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-    // plugin's task(s), then test the result.
-    grunt.registerTask('test', ['clean', 'copy', 'ngsrc', 'nodeunit']);
-
-    // By default, lint and run all tests.
-    grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('default', ['lint', 'test']);
 
 };
--- tasks/ngsrc.js
+++ tasks/ngsrc.js
@@ -8,140 +8,144 @@
 
 'use strict';
 
-var _ = require('lodash'),
-    path = require('path'),
-    replace = require('replace');
-
+var replace = require('replace');
 
 module.exports = function(grunt) {
 
-    grunt.registerMultiTask(
-        'ngsrc',
-        'Find and add Angular.js files to index.html automatically and in correct order (also applicable for source files of any other framework). Great for development builds, inspired by usemin which is strongly recommended for handling of production builds.',
-        function () {
-            var options = this.options({
-                moduleDiscriminator: 'module.js',
-                path: undefined,
-                placeholder: '<!-- ngsrc -->'
-            });
-            this.files.forEach(function(files) {
-                var replacementPaths = getReplacementPaths(getSortedPaths(files.src)),
-                    replacement = createTemplate(replacementPaths),
-                    destinations = getDestinations(files.dest);
-                if (!destinations.length) {
-                    grunt.log.writeln('No destination files found');
-                    return;
-                }
-                if (!replacementPaths.length) {
-                    grunt.log.writeln('No src files found');
-                    return;
-                }
-                logVerbose(replacementPaths, destinations);
-                grunt.log.write('Adding ' + replacementPaths.length + ' script tags into ' + destinations.length + ' destination ' + (destinations.length === 1 ? 'file' : 'files') + ' ');
-                replace({
-                    regex: options.placeholder,
-                    replacement: replacement,
-                    paths: destinations,
-                    recursive: true,
-                    silent: true
-                });
-                grunt.log.ok();
-            });
+  var TASK_NAME = 'ngsrc';
+  var TASK_DESCRIPTION = 'Find and add Angular.js files to index.html automatically and in ' +
+    'correct order (also ' + 'applicable for source files of any other ' +
+    'framework). Great for development builds, inspired by usemin which ' +
+    'is strongly recommended for handling of production builds.';
 
-            function getSortedPaths(paths) {
-                var pathsModules = [],
-                    pathsOther = [];
-                paths.forEach(function(path) {
-                    if (getFilename(path).indexOf(options.moduleDiscriminator) > -1) {
-                        pathsModules.push(path);
-                    } else {
-                        pathsOther.push(path);
-                    }
-                });
-                pathsModules.sort(sortByDepth);
-                pathsOther.sort(sortByDepth);
-                return pathsModules.concat(pathsOther);
+  grunt.registerMultiTask(TASK_NAME, TASK_DESCRIPTION, function() {
+    var options = this.options({
+      moduleDiscriminator: 'module.js',
+      path: undefined,
+      placeholder: '<!-- ngsrc -->'
+    });
+    this.files.forEach(function(files) {
+      var replacementPaths = getReplacementPaths(getSortedPaths(files.src));
+      var replacement = createTemplate(replacementPaths);
+      var destinations = getDestinations(files.dest);
+      if (!destinations.length) {
+        grunt.log.writeln('No destination files found');
+        return;
+      }
+      if (!replacementPaths.length) {
+        grunt.log.writeln('No src files found');
+        return;
+      }
+      logVerbose(replacementPaths, destinations);
+      grunt.log.write('Adding ' + replacementPaths.length + ' script tags into ' +
+                      destinations.length + ' destination ' +
+                      (destinations.length === 1 ? 'file' : 'files') + ' ');
+      replace({
+        regex: options.placeholder,
+        replacement: replacement,
+        paths: destinations,
+        recursive: true,
+        silent: true
+      });
+      grunt.log.ok();
+    });
 
-                function sortByDepth(p1, p2){
-                    if (hasDepth(p1) && hasDepth(p2)) {
-                        if (getDepth(p1) < getDepth(p2)) {
-                            return -1;
-                        }
-                        if (getDepth(p1) > getDepth(p2)) {
-                            return 1;
-                        }
-                    } else {
-                        return 0;
-                    }
-                }
+    function getSortedPaths(paths) {
+      var pathsModules = [];
+      var pathsOther = [];
+      paths.forEach(function(path) {
+        if (getFilename(path).indexOf(options.moduleDiscriminator) > -1) {
+          pathsModules.push(path);
+        } else {
+          pathsOther.push(path);
+        }
+      });
+      pathsModules.sort(sortByDepth);
+      pathsOther.sort(sortByDepth);
+      return pathsModules.concat(pathsOther);
 
-                function hasDepth(path) {
-                    return path && path.split && path.split('/').length;
-                }
+      function sortByDepth(p1, p2) {
+        if (hasDepth(p1) && hasDepth(p2)) {
+          if (getDepth(p1) < getDepth(p2)) {
+            return -1;
+          }
+          if (getDepth(p1) > getDepth(p2)) {
+            return 1;
+          }
+        } else {
+          return 0;
+        }
+      }
 
-                function getDepth(path) {
-                    return path.split('/').length;
-                }
+      function hasDepth(path) {
+        return path && path.split && path.split('/').length;
+      }
 
-                function getFilename(path) {
-                    if (hasDepth(path)) {
-                        var tokens = path.split('/');
-                        return tokens[tokens.length - 1];
-                    }
-                    return path;
-                }
-            }
+      function getDepth(path) {
+        return path.split('/').length;
+      }
 
-            function getReplacementPaths(paths) {
-                var replacementPaths = [];
-                paths.forEach(function(p) {
-                    if (p && p.split && p.split('/').length) {
-                        var origPathArray = p.split('/'),
-                            origFileName = origPathArray[origPathArray.length - 1],
-                            origPathToFile = origPathArray.splice(0, origPathArray.length - 1),
-                            pathToFile = options.path ? options.path + origPathToFile.join('/') + '/' : origPathToFile.join('/') + '/',
-                            replacementPath = pathToFile + origFileName;
-                        replacementPaths.push(replacementPath);
-                    }
-                });
-                return replacementPaths;
-            }
+      function getFilename(path) {
+        if (hasDepth(path)) {
+          var tokens = path.split('/');
+          return tokens[tokens.length - 1];
+        }
+        return path;
+      }
+    }
 
-            function createTemplate(replacementPaths) {
-                var result;
-                replacementPaths.forEach(function(path, index) {
-                    result = result ? result : '\n';
-                    result += ('<script src="' + path + '"></script>\n');
-                });
-                result = result ? result : options.placeholder;
-                return grunt.util.normalizelf(result);
-            }
+    function getReplacementPaths(paths) {
+      var replacementPaths = [];
+      paths.forEach(function(p) {
+        if (p && p.split && p.split('/').length) {
+          var origPathArray = p.split('/');
+          var origFileName = origPathArray[origPathArray.length - 1];
+          var origPathToFile = origPathArray.splice(0, origPathArray.length - 1);
+          var pathToFile = options.path ?
+          options.path + origPathToFile.join('/') + '/' :
+          origPathToFile.join('/') + '/';
+          var replacementPath = pathToFile + origFileName;
+          replacementPaths.push(replacementPath);
+        }
+      });
+      return replacementPaths;
+    }
 
-            function getDestinations(paths) {
-                var destinations = [];
-                if (paths && paths.length) {
-                    paths.forEach(function(p) {
-                        grunt.file.expand(p).forEach(function(destPath) {
-                            destinations.push(destPath);
-                        });
-                    });
-                }
-                return destinations;
-            }
+    function createTemplate(replacementPaths) {
+      var result;
+      replacementPaths.forEach(function(path, index) {
+        result = result ? result : '\n';
+        result += ('<script src="' + path + '"></script>\n');
+      });
+      result = result ? result : options.placeholder;
+      return grunt.util.normalizelf(result);
+    }
 
-            function logVerbose(replacementPaths, destinations) {
-                replacementPaths.forEach(function (path) {
-                    grunt.verbose.write('Src: ' + path + '...');
-                    grunt.verbose.ok();
-                });
-                grunt.verbose.writeln();
-                destinations.forEach(function (path) {
-                    grunt.verbose.write('Dest: ' + path + '...');
-                    grunt.verbose.ok();
-                });
-                grunt.verbose.writeln();
-            }
-
+    function getDestinations(paths) {
+      var destinations = [];
+      if (paths && paths.length) {
+        paths.forEach(function(p) {
+          grunt.file.expand(p).forEach(function(destPath) {
+            destinations.push(destPath);
+          });
         });
+      }
+      return destinations;
+    }
+
+    function logVerbose(replacementPaths, destinations) {
+      replacementPaths.forEach(function(path) {
+        grunt.verbose.write('Src: ' + path + '...');
+        grunt.verbose.ok();
+      });
+      grunt.verbose.writeln();
+      destinations.forEach(function(path) {
+        grunt.verbose.write('Dest: ' + path + '...');
+        grunt.verbose.ok();
+      });
+      grunt.verbose.writeln();
+    }
+
+  });
 
 };
